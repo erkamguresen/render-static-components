17260161c6ca5e713d58aa14d03d0d4587ceb0e7
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-01 12:23:52 +0200

    user part is done

--- after/components/userMainDiv.js
+++ after/components/userMainDiv.js
@@ -1,10 +1,23 @@
+import { divElement } from './div.js';
+import { imgElement } from './img.js';
+
 export const userDiv = (className = '', idName = '') => {
   const newUserDiv = document.createElement('div');
 
   newUserDiv.className = className;
   newUserDiv.id = idName;
 
-  //TODO change it later
-  newUserDiv.innerText = 'User Part';
+  const avatarDiv = divElement('avatar');
+  avatarDiv.appendChild(imgElement('../images/posts/default-user.png', 'User'));
+
+  newUserDiv.appendChild(avatarDiv);
+
+  const descDiv = divElement('desc');
+  descDiv.innerHTML = `<a href="../https://github.com/leocosta1" target="_blank"
+  >leocosta1</a>
+  <span>Always learning!</span>`;
+
+  newUserDiv.appendChild(descDiv);
+
   return newUserDiv;
 };
